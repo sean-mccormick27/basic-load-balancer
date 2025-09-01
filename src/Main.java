9232374a17a88850f82534d819153f34ad31bff6
Sean McCormick <sean.mccormick27@gmail.com>
2025-09-01 16:48:10 +0100

    Entry-point main class with example backend services and other parameters for load balancer

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         int listenPort = 49152;
-        BackendService backendService1 = new BackendService("service_1", 5000);
-        BackendService backendService2 = new BackendService("service_2", 6000);
-        backendService2.setOnline(false);
-        BackendService backendService3 = new BackendService("service_3", 7000);
+        int healthCheckInterval = 5000;
+        BackendService backendService1 = new BackendService("localhost", 5000);
+        BackendService backendService2 = new BackendService("localhost", 6000);
         BackendService[] backendServices = {
-                backendService1, backendService2, backendService3
+                backendService1, backendService2
         };
-        new LoadBalancer(listenPort, Arrays.asList(backendServices)).start();
+        new LoadBalancer(listenPort, healthCheckInterval, Arrays.asList(backendServices)).start();
     }
 }
