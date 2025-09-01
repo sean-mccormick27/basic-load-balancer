d338c0727d97d6e07c86a448f61cba574a51ff91
Sean McCormick <sean.mccormick27@gmail.com>
2025-09-01 14:56:25 +0100

    Javadocs added to ClientConnectionHandler class

--- src/ClientConnectionHandler.java
+++ src/ClientConnectionHandler.java
@@ -3,18 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 /**
- * A class used to represent a TCP client connection in its own thread.
+ * A class used to handle single client connection in its own thread.
+ * This class is responsible reads messages sent by a client over a socket
+ * and will later send them to a specified back-end service.
+ * The connection will be closed once the client disconnects or if an
+ * error occurs during communication.
  */
 public class ClientConnectionHandler implements Runnable {
 
+    /** The socket communicating with the connected client.*/
     private final Socket clientSocket;
+
+    /** The name of the backend service where messages from the client will be sent.*/
     private final String backendServiceName;
 
+    /**
+     * Creates a new handler for a client connection.
+     *
+     * @param clientSocket       the socket representing the client's TCP connection
+     * @param backendServiceName the name of the backend service where client messages
+     *                           will be sent to.
+     */
     public ClientConnectionHandler(Socket clientSocket, String backendServiceName) {
         this.clientSocket = clientSocket;
         this.backendServiceName = backendServiceName;
     }
 
+    /**
+     * Processes incoming messages from the client, sending them to the
+     * backend service with the given name
+     */
     public void run() {
         String clientAddress = clientSocket.getInetAddress().toString();
         System.out.println("Client connected: [" + clientAddress + "]");
