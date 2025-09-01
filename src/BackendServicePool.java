c622a1ac285a2334b94678dfe73d0319e1b397df
Sean McCormick <sean.mccormick27@gmail.com>
2025-09-01 11:51:37 +0100

    Additional javadocs written for BackendServicePool class

--- src/BackendServicePool.java
+++ src/BackendServicePool.java
@@ -10,16 +10,39 @@ import java.util.List;
  */
 public class BackendServicePool {
 
+    /**
+     * A list representing the pool of {@link BackendService} objects that
+     * a TCP message can be forwarded to.
+     */
     private final List backendServices;
 
+    /**
+     * Constructs a {@link BackendServicePool} object with a given
+     * list of {@link BackendService} objects for selection.
+     *
+     * @param backendServices the list of backend services available
+     *                        for selection.
+     */
     public BackendServicePool(List backendServices) {
         this.backendServices = backendServices;
     }
 
+    /**
+     * Selects the {@link BackendService} where an incoming TCP message is
+     * to be forwarded to. For now, the first object in the pool is selected.
+     *
+     * @return the selected {@link BackendService} object.
+     */
     public BackendService selectBackendService() {
+        //TODO: Write a more sophisticated selection algorithm.
         return (BackendService) backendServices.get(0);
     }
 
+    /**
+     * Gets the list of {@link BackendService} objects in the pool.
+     *
+     * @return the list of {@link BackendService} objects in the pool.
+     */
     public List getBackendServices() {
         return backendServices;
     }
